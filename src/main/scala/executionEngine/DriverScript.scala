7642baca3b87c1c7b837914414fb3f9e51ba2467
bluesky271 <ngoulong2001@yahoo.com.hk>
2018-12-11 22:17:23 +0000

    fix loop to run the correct amount of test steps

--- src/main/scala/executionEngine/DriverScript.scala
+++ src/main/scala/executionEngine/DriverScript.scala
@@ -6,7 +6,7 @@ import utility.ExcelUtils
 object DriverScript extends ExcelUtils {
 
   def main(args: Array[String]): Unit = {
-    for (iRow <- 1 to 5 by 1) {
+    for (iRow <- 1 to 4 by 1) {
       val sActionKeyword: String = getCellData(iRow, 3)
 
       sActionKeyword match {
